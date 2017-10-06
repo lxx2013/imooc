0ac37eb18ca03e2b39657aa853da4f5c6cd67621
lxx <690154577@qq.com>
2017-10-06 22:21:34 +0800

    学习使用Vue并将课程course部分模块化，初步完成焦点轮播图的分块
      其中，course部分目前只录入了3门课程。焦点轮播图部分完全是自己设计的DOM结构，不过由于时间紧张，a标签的扩展页面暂时使用图片代替。学习了一个很重要的知识：z-index层叠上下文！划重点！以后抽空要整理出来！

---
+++ js/content-vue.js
@@ -0,0 +1,275 @@
+var course0 = new Vue({
+	el: '#Shizhan',
+	data: {
+		courses: [
+			{
+				label: ['Python', 'Java'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course.jpg',
+				content: 'Python3入门与进阶',
+				info: ['实战', '初级'],
+				users: 385,
+				price: '￥366.00'
+	  		},
+			{
+				label: ['Node.js', 'Vue.js'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course2.jpg',
+				content: 'Get全栈技能点Vue2.0/Node.js/MongoDB',
+				info: ['实战', '中级'],
+				users: 713,
+				price: '￥348.00'
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+			}
+    		]
+	}
+});
+var course1 = new Vue({
+	el: '#Xinshang',
+	data: {
+		courses: [
+			{
+				label: ['Python', 'Java'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course.jpg',
+				content: 'Python3入门与进阶',
+				info: ['实战', '初级'],
+				users: 385,
+				price: '￥366.00'
+	  		},
+			{
+				label: ['Node.js', 'Vue.js'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course2.jpg',
+				content: 'Get全栈技能点Vue2.0/Node.js/MongoDB',
+				info: ['实战', '中级'],
+				users: 713,
+				price: '￥348.00'
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+			},
+			{
+				label: ['Python', 'Java'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course.jpg',
+				content: 'Python3入门与进阶',
+				info: ['实战', '初级'],
+				users: 385,
+				price: '￥366.00'
+	  		},
+			{
+				label: ['Node.js', 'Vue.js'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course2.jpg',
+				content: 'Get全栈技能点Vue2.0/Node.js/MongoDB',
+				info: ['实战', '中级'],
+				users: 713,
+				price: '￥348.00'
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				hot: true,
+				free: true
+			}
+    ]
+	}
+})
+var course2 = new Vue({
+	el: '#Xinshou',
+	data: {
+		courses: [
+			{
+				label: ['Python', 'Java'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course.jpg',
+				content: 'Python3入门与进阶',
+				info: ['实战', '初级'],
+				users: 385,
+				price: '￥366.00'
+	  		},
+			{
+				label: ['Node.js', 'Vue.js'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course2.jpg',
+				content: 'Get全栈技能点Vue2.0/Node.js/MongoDB',
+				info: ['实战', '中级'],
+				users: 713,
+				price: '￥348.00'
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+			},
+			{
+				label: ['Python', 'Java'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course.jpg',
+				content: 'Python3入门与进阶',
+				info: ['实战', '初级'],
+				users: 385,
+				price: '￥366.00'
+	  		},
+			{
+				label: ['Node.js', 'Vue.js'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course2.jpg',
+				content: 'Get全栈技能点Vue2.0/Node.js/MongoDB',
+				info: ['实战', '中级'],
+				users: 713,
+				price: '￥348.00'
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				new: true,
+				free: true
+	  		},
+			{
+				label: ['Java', 'SpringBoot'],
+				href: "http://coding.imooc.com/class/136.html?mc_marking=b587280c0c1c0e76c1092aa21406565a&mc_channel=syb6",
+				img: 'course3.jpg',
+				content: 'Java SpringBootSSM到Spring Boot-从零开发校园商铺平台实战初级',
+				info: ['实战', '初级'],
+				users: 48,
+				price: '￥299.00',
+				hot: true,
+			}
+    ]
+	}
+})
